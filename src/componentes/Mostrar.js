1ebfddfcbb9ba33f167ace78eb964f42b2393802
Elton Vilanculos <elltonvillas10@gmail.com>
2020-09-11 19:09:36 +0200

    jsc

--- src/componentes/Mostrar.js
+++ src/componentes/Mostrar.js
@@ -1,24 +1,70 @@
 import React from 'react';
 
-const Mostrar = () => {
+class Mostrar extends React.Component{
 
-    const buscaFactos = () => {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            facts : [] ,
+            loading:true
+        }
     }
 
-    return (
-        <div id="modal1" className="modal" style={{ display: "none" }}>
-            <div className="overlay"></div>
-            <div className="modal_content">
-                <h2>Ouse Duvidar</h2>
-                <p>O céu é azul.</p>
-                <p>A água não tem cor.</p>
-                <p>Ninguem é perfeito.</p>
-                <p>A terra não é plana.</p>
-                <button title="Close" className="close_modal">x</button>
+      async componentDidMount() {
+
+          const URL = 'http://127.0.0.1:8002/v1/data';
+          const response = await fetch(URL);
+          const json = await response.json();
+          const facts =   json ;
+          console.log(json ??'loading')
+          this.setState({facts:facts , loading:false} )
+
+
+
+      }
+
+
+    render() {
+        if(this.state.facts.length<=0){
+            return (<div id="modal1" className="modal" style={{ display: "none" }}>
+                <div className="overlay"></div>
+                <div className="modal_content">
+
+                    <h2>Ouse Duvidar</h2>
+
+
+                    <p> Oops , nothing to show </p>
+
+
+
+                    <button title="Close" className="close_modal">x</button>
+                </div>
+            </div>)
+        }
+        return (
+            <div id="modal1" className="modal" style={{ display: "none" }}>
+                <div className="overlay"></div>
+                <div className="modal_content">
+
+                    <h2>Ouse Duvidar</h2>
+
+
+                    {this.state.loading  ? ( <p>Carregando ...</p> ) :  (<div>{this.state.facts.map(fact=>(
+                       <div key={fact.id}>
+                        <p>{fact.description}</p>
+                       </div>
+                        )
+
+                    )}</div>)}
+
+                    <button title="Close" className="close_modal">x</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+
 }
 
 export default Mostrar;
--- src/componentes/Registar.js
+++ src/componentes/Registar.js
@@ -1,19 +1,74 @@
 import React from 'react';
 
-const Registar = () => {
-    return (
-        <div id="modal2" className="modal" style={{ display: "none" }}>
-            <div className="overlay"></div>
-            <div className="modal_content">
-                <h2>Não Minta</h2>
-                <form id="factsForm" method="POST" action="#">
-                    <textarea placeholder="Informe um facto" rows="2" cols="3"></textarea>
-                    <button type="submit" id="addButton">Adicionar</button>
-                </form>
-                <button title="Close" className="close_modal">x</button>
-            </div>
-        </div>
-    );
+class Registar extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '' ,
+            success : 'Adicionar'
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+
+        this.setState({value: event.target.value});
+
+    }
+
+    handleSubmit(event) {
+
+
+        this.setFact(this.state.value)
+        event.preventDefault();
+    }
+
+    async setFact(text) {
+        this.setState({success :  'Aguarde ... ! '})
+
+        try {
+            const requestOptions = {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                // body:{desc:'my fact'}
+
+            };
+            const response = await fetch(`http://127.0.0.1:8002/v1/data?desc=${text}`, requestOptions);
+            const data = await response.json();
+
+            if (data.status) {
+                this.setState({success: 'Inserido com sucesso ! '})
+            } else {
+                this.setState({success: 'Falha ao inserir  ! '})
+            }
+
+        }catch (e) {
+
+            console.log(e)
+            this.setState({success: 'Verifique a conexão com o servidor ! '})
+        }
+
+
+    }
+ render() {
+     return (
+         <div id="modal2" className="modal" style={{ display: "none" }}>
+             <div className="overlay"></div>
+             <div className="modal_content">
+                 <h2>Não Minta</h2>
+                 <form id="factsForm" method="POST" action="#" onSubmit={this.handleSubmit}>
+                     <textarea placeholder="Informe um facto" rows="2" cols="3" value={this.state.value} onChange={this.handleChange} />
+                     <button type="submit" id="addButton">{this.state.success}</button>
+                 </form>
+                 <button title="Close" className="close_modal">x</button>
+             </div>
+         </div>
+     );
+ }
+
+
 }
 
 export default Registar;
